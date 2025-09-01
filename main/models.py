ff5db4edd55fe2430d7b82efbbb7de9bea451fac
pushpaks28 <167504024+pushpaks28@users.noreply.github.com>
2025-09-01 21:42:20 +0100

    migration

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+
+class Document(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete= models.CASCADE)
+    title = models.CharField(default="Title",max_length=30)
+    content = models.TextField(blank=True , null=True)
+    created_at = models.DateTimeField(auto_now_add=True) # db auto update the field when its added
+    updated_at = models.DateTimeField(auto_now=True) # db auto update the field whenever the field is updated
+
+    def save(self , *args , **kwargs):
+        super().save(*args,**kwargs)
